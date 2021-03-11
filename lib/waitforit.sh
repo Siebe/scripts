842673caa555eb5949e457653971c7cfb20b42ec
Siebe <siebe@pluxbox.nl>
2021-03-11 23:39:26 +0100

    I told the witch doctor...

--- lib/waitforit.sh
+++ lib/waitforit.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 usage () {
-  echo 'Wait for certain output of something and stop'
-	echo ' ./waitforit.sh [-tFStmrkv] [-c COMMAND] [-t SLEEPTIME] [-T TIMEOUT] [-R RETRIES] [-K SIGNAL] -- TESTSTRING'
-	echo 'TESTSTRING = string to test with'
-	echo '-c COMMAND = command to run and repeat, if omitted waitforit.sh will listen to lines from STDIN'
-	echo '-t SLEEPTIME = seconds to sleep between command repeat, 0 means no repeat (default 0)'
-	echo '-T TIMEOUT = stop after TIMEOUT seconds'
-	echo '-R RETRIES = stop after repeating RETRIES times'
-	echo '-F = ignore command failure (keep repeating on failure exit codes)'
-	echo '-S = ignore STDERR from command, else take STDERR in the result to be tested'
-	echo '-m = multiline mode, command only'
-	echo '-r = regex mode, no multiline'
-	echo '-k = kill parent'
-	echo '-K SIGNAL = kill signal, what will kill your parent? (default: 1)'
-	echo '-v = verbose mode'
+  if [ -z "$quiet" ]; then
+    echo 'Wait for certain output of something and stop it... yeah just like that.'
+    echo ' ./waitforit.sh [-tFStmrkv] [-c COMMAND] [-t SLEEPTIME] [-T TIMEOUT] [-R RETRIES] [-K SIGNAL] -- TESTSTRING'
+    echo 'TESTSTRING = string to test with'
+    echo '-c COMMAND = command to run and repeat, if omitted listen to lines from STDIN'
+    echo '-t SLEEPTIME = seconds to sleep between command repeat, 0 means no repeat (default 0)'
+    echo '-T TIMEOUT = stop after TIMEOUT seconds, 0 is no timeout (default 0)'
+    echo '-R RETRIES = stop after repeating RETRIES times (default infinite)'
+    echo '-F = ignore command failure and keep repeating after non-zero exit codes'
+    echo '-S = ignore STDERR from command, else take STDERR in the result to be tested'
+    echo '-m = multiline mode, command only'
+    echo '-r = regex mode, TESTSTRING is now simple regex pattern; no multiline'
+    echo '-k = king mode, kill parent process(es)'
+    echo '-K SIGNAL = kill signal, choose your murder weapon (default: 1)'
+    echo '-v = verbose mode'
+    echo '-q = quiet mode, combine with -k for ninja mode'
+  fi
 }
 
 my_pid=$$
@@ -25,7 +28,7 @@ kill_signal=1
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-while getopts ":hc:t:T:R:FSmrkK:v" options
+while getopts ":hc:t:T:R:FSmrkK:vq" options
 do
 	case $options in
 			h ) usage; examples; exit 0;;
@@ -40,19 +43,33 @@ do
       k ) kill_parent=1;;
       K ) kill_signal="${OPTARG}";;
       v ) verbose=1;;
-      ? ) echo 'Error: invalid option'; usage; exit 1;;
+      q ) quiet=1;;
+      ? ) >&2 echo 'Error: invalid option'; usage; exit 1;;
 	esac
 done
 
 shift $(($OPTIND - 1))
 expected_string="$@"
 
-[ -z "$expected_string" ] && { >&2 echo 'Error: TESTSTRING required'; usage; exit 1; }
-[ -n "$multiline" ] && [ -n "$regex" ] && { >&2 echo 'Error: Can not use regex and multiline mode simultaneously'; usage; exit 1; }
-[ -n "$command" ] && [ -n "$kill_parent" ] && unset kill_parent && echo "Warning: can not kill parent when running command as child"
-[ -n "$command" ] && [ "$sleep_time" -lt 1 ] && sleep_time=1 && echo "Warning: can not kill parent when running command as child"
-[ -z "$command" ] && [ -n "$multiline" ] && unset multiline && echo "Warning: can not use multiline with stdin"
+[ -n "$quiet" ] && unset debug
+[ -z "$expected_string" ] && { [ -z "$quiet" ] && >&2 echo 'Error: TESTSTRING required'; usage; exit 1; }
+[ -n "$multiline" ] && [ -n "$regex" ] && { [ -z "$quiet" ] && >&2 echo 'Error: Can not use regex and multiline mode simultaneously'; usage; exit 1; }
+if [ -n "$command" ]; then
+  [ -n "$kill_parent" ] && unset kill_parent && [ -z "$quiet" ] && echo "Warning: can not kill parent when running command as child"
+  [ "$sleep_time" -lt 1 ] && sleep_time=1 && [ -z "$quiet" ] && echo "Warning: SLEEPTIME can not be less than 1"
+else
+  if [ "$kill_signal" -lt 0] || [ "$kill_signal" -gt 64]; then
+    [ -z "$quiet" ] && >&2 echo 'Error: invalid KILLSIGNAL'; usage; exit 1;
+  fi
+  [ -n "$multiline" ] && unset multiline && [ -z "$quiet" ] && echo "Warning: can not use multiline with in stdin mode"
+  if [ -n "$ignore_fail" ] || [ -n "$ignore_stderr" ]; then
+    unset $ignore_fail
+    unset $ignore_stderr
+    [ -z "$quiet" ] && echo "Warning: can not ignore failure or stderr in stdin mode"
+  fi
+fi
 
+#always redirect the stderr of command
 if [ -n "$command" ]; then
   full_command=$command
   if [ -n "$ignore_stderr" ]; then
@@ -70,10 +87,18 @@ if [ -n "$verbose" ]; then
     echo "${bold}Sleep Time        : ${sleep_time} seconds"
     echo -n "${bold}Timeout           : "; [ -z "$timeout" ] && echo "none" || echo "$timeout seconds${normal}"
     echo -n "${bold}Max Repeats       : "; [ -z "$max_repeat" ] && echo "infinite" || echo "$max_repeat${normal}"
-
+    echo -n "${bold}Ignore Fail       : "; [ -z "$ignore_fail" ] && echo "no" || echo "yes${normal}"
+    echo -n "${bold}Ignore stderr     : "; [ -z "$ignore_stderr" ] && echo "no" || echo "yes${normal}"
+    echo -n "${bold}Multiline         : "; [ -z "$multiline" ] && echo "no" || echo "yes${normal}"
+  else
+    echo -n "${bold}Kill parents      : "; [ -z "$kill_parent" ] && echo "no" || echo "yes${normal}"
+    echo "${bold}Kill signal       : ${kill_signal}"
   fi
+  echo -n "${bold}Regex             : "; [ -z "$regex" ] && echo "no" || echo "yes${normal}"
+  echo "2${bold}pid              : ${my_pid}${normal}"
 fi
 
+#run before exiting
 finish () {
   if [ -n "$timeout_pid" ] && ( ps -p $timeout_pid > /dev/null ); then
     [ -n "$verbose" ] && echo "${bold}killing timeout process $timeout_pid${normal}"
@@ -93,13 +118,15 @@ on_term () {
 # Execute function on_term() receiving TERM signal
 trap 'on_term' TERM
 
+#run a string test
 runTest () {
   test_lines=$1
   expected_string=$2
   if [ -z "$multiline" ]; then
     while IFS= read -r test_line; do
         [ -n "$verbose" ] && echo -n "${bold}Testing line: ${normal}${test_line}" && echo "${bold} against expected: ${normal}${expected_string}"
-        if [ "$test_line" = "$expected_string" ]; then
+        if ( [ -n "$regex" ] && ( echo "$test_line" | grep "$expected_string" -c 2> /dev/null 1> /dev/null ) )\
+        || ( [ -z "$regex" ] && [ "$test_line" = "$expected_string" ] ); then
            [ -n "$verbose" ] && echo "${bold}Match found, goodbye!${normal}"
           finish
         fi
@@ -113,11 +140,12 @@ runTest () {
   fi
 }
 
+#start a separate timeout process that will kill main process
 runTimeOut () {
   time=$1
   pid=$2
   sleep $time
-  [ -n "$verbose" ] && echo "${bold}Timed out, killing PID $pid${normal}"
+  [ -z "$quiet" ] && echo "${bold}Timed out, killing pid $pid${normal}"
   ( ps -p $pid > /dev/null ) && kill $pid
 }
 
@@ -127,31 +155,34 @@ if [ -n "$timeout" ] && [ "$timeout" -gt 0 ]; then
   [ -n "$verbose" ] && echo "${bold}Timeout process has pid $timeout_pid${normal}"
 fi
 
+#Start processing here
 if [ -n "$command" ]; then
+  #Ether run a command in a loop, check the result per line or multiline and sleep every loop
   while [ -z "$result_found" ]; do
-    echo "${bold}Running: ${normal}${full_command}"
-    result=$(eval $full_command)
+    [ -z "$quiet" ] && echo "${bold}Running : ${normal}${full_command}"
+    result=$(eval "$full_command")
     exit_code="$?"
-    echo "${bold}Result: ${normal}${result}"
+    [ -z "$quiet" ] && echo "${bold}Result  : ${normal}${result}"
     if [ -z "$ignore_fail" ] && [ "$exit_code" -ne 0 ]; then
-      echo "${bold}Command failed with exit code ${exit_code}${normal}"
+      [ -z "$quiet" ] && echo >&2 "${bold}Command failed with exit code ${exit_code}${normal}"
       exit $exit_code
     fi
     runTest "$result" "$expected_string"
     ( [ "$sleep_time" -lt 1 ] || ( [ -n "$max_repeat" ] && [ "$max_repeat" -eq 1 ] ) ) && finish
     if [ -n "$max_repeat" ]; then
-      let "repeats=repeats+1"
       if [ "$repeats" -ge "$max_repeat" ]; then
-         echo "${bold}Stopping after ${repeats} repeats${normal}"
+        [ -z "$quiet" ] && echo "${bold}Stopping after ${repeats} repeats${normal}"
         finish
       fi
+      let "repeats=repeats+1"
     fi
     [ -n "$verbose" ] && echo "${bold}Repeating in ${sleep_time} seconds${normal}"
     sleep $sleep_time
   done
 else
+  #Or take lines from stdin in a loop and check these
   while read input_line && [ -z "$result_found" ]; do
-    echo "${bold}Input line: ${normal}${input_line}"
+    [ -z "$quiet" ] && echo "${bold}Input line: ${normal}${input_line}"
     runTest "$input_line" "$expected_string"
   done
 fi
