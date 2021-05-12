62d0aaa4a436a82836ef970310fac5bbc4405dc5
Siebe <siebe@pluxbox.nl>
2021-05-12 23:16:40 +0200

    krestart fix

--- k8s/krestart.sh
+++ k8s/krestart.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 pattern=''
-type=''
 
 if [ $# -eq 1 ] ; then
     pattern=$1
-    type='deployment'
+    nsflag="--all-namespaces"
 fi
 if [ $# -eq 2 ] ; then
     pattern=$2
-    type=$1
+    namespace=$1
+    nsflag="-n '{namespace}'"
 fi
 
-matching=`kubectl get $type --all-namespaces | grep -iP "^([^\s]+)\s+([^\s]*$pattern[^\s]*)\s"` 
+matching=`kubectl get deployment ${nsflag} | grep -ioP "^([^\s]+)\s+([^\s]*${pattern}[^\s]*)\s"` 
 
 while IFS= read -r match; do
   entry=("$match")
   echo --- Restarting ${entry[1]} \(${entry[0]}\): ---
-  kubectl rollout restart $type -n${entry[0]} ${entry[1]} 
+  kubectl rollout restart deployment -n${entry[0]} ${entry[1]} 
 done <<< "$matching"
