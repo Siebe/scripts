29881faaed95b3713bfccce1f23b2670c3473fc6
Siebe <siebe@pluxbox.nl>
2021-05-12 23:18:30 +0200

    kssh update

--- pb/kssh.sh
+++ pb/kssh.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -z "$WHATSMYIP" ] || [ "$WHATSMYIP" != "$KANTOORIP" ]; then
-  ssh -o ProxyCommand="ssh -W %h:%p siebe@${KANTOORIP}" $@
-else
-  ssh $@
+if [ -z "$@" ]; then
+  ssh siebe@${KANTOORIP}
+  exit 0
 fi
+
+if [ -z "$WHATSMYIP" ] || [ "$WHATSMYIP" != "$KANTOORIP" ]; then
+  ssh -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p siebe@${KANTOORIP}" $@
+  exit 0
+fi
+
+ssh $@
