964391f0d4e13f27d25c8f03ee220630307fccf5
Siebe <siebe@pluxbox.nl>
2021-11-10 00:00:32 +0100

    back to cbacklight plus link to instructions how to set rights properly

--- i3/brightness.py
+++ i3/brightness.py
@@ -4,13 +4,19 @@ import os;
 import sys;
 
 #For cbacklight to work you might need to:
+
+
 # sudo chown root:video /sys/class/backlight/intel_backlight/brightness
 # sudo chmod 664 /sys/class/backlight/intel_backlight/brightness
-#getBacklightPercentCommand = '$SCRIPT_DIR/i3/cbacklight | grep -Po "\d*(\.\d+)?"'
-#setBacklightPercentCommand = '$SCRIPT_DIR/i3/cbacklight --set '
 
-getBacklightPercentCommand = 'xbacklight'
-setBacklightPercentCommand = 'xbacklight -set '
+# or 
+# https://superuser.com/questions/484678/cant-write-to-file-sys-class-backlight-acpi-video0-brightness-ubuntu
+
+getBacklightPercentCommand = '$SCRIPT_DIR/i3/cbacklight | grep -Po "\d*\.\d+"'
+setBacklightPercentCommand = '$SCRIPT_DIR/i3/cbacklight --set '
+
+# getBacklightPercentCommand = 'xbacklight'
+# setBacklightPercentCommand = 'xbacklight -set '
 
 brightness = round(float(os.popen(getBacklightPercentCommand).read()));
 print("Current brightness: "+str(brightness));
@@ -32,7 +38,7 @@ elif (brightness == 0.0):
         multiply = 100;
 
 
-multiply = float(multiply / 100.0); 
+multiply = float(multiply / 100.0);
 
 print(str(brightness)+" * "+str(multiply));
 brightness *= multiply;
