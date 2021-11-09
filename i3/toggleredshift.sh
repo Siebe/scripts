9c302298c1642bcc106e56a0b1877f76c7bd58bc
Siebe <siebe@pluxbox.nl>
2021-11-10 00:01:00 +0100

    toggleredshift multiple monitors fix

--- i3/toggleredshift.sh
+++ i3/toggleredshift.sh
@@ -2,7 +2,7 @@
 REDSHIFT_OFF=$(xrandr --verbose | grep Gamma.*1.0:1.0:1.0 | wc -l)
 echo $REDSHIFT_ON
 
-if [ "1" -eq "$REDSHIFT_OFF" ]
+if [ "0" -ne "$REDSHIFT_OFF" ]
 then
 	echo "redshift on"
 	redshift -O 3500 2>&1 > /dev/null 
