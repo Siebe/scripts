623de72e5e3242e48747460fb28640e666ef430e
Siebe <siebe@pluxbox.nl>
2021-11-10 22:38:42 +0100

    renamed to displays, now 3 modes and in i3config

---
+++ i3/displays.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+monitor_state=$(cat /tmp/i3script_monitorstate 2> /dev/null || echo "right-of")
+main_monitor=$(xrandr --listactivemonitors | grep -P "^\s*0:" | grep -Po "[\w-]+$")
+all_monitors=($(xrandr | grep ' connected ' | grep -Po "^[\w-]+"))
+orientation=""
+
+#echo "main: $main_monitor"
+#echo "all_monitors: ${all_monitors[*]}"
+
+if [ -n "${all_monitors[1]}" ]; then
+  [ -n "$main_monitor" ] && orientation="--${monitor_state} ${main_monitor}"
+  command="xrandr --output ${all_monitors[1]} --auto ${orientation}"
+  #echo $command
+  $command
+fi
+
+inverted_state=$monitor_state
+[ "$monitor_state" == "right-of" ] && inverted_state='left-of'
+[ "$monitor_state" == "left-of" ] && inverted_state='right-of'
+
+
+if [ -n "${all_monitors[2]}" ]; then
+  [ -n "$main_monitor" ] && orientation="--${inverted_state} ${main_monitor}"
+  [ -z "$main_monitor" ] && orientation="--${monitor_state} ${all_monitors[1]}"
+  xrandr --output ${all_monitors[2]} --auto ${orientation}
+fi
+
+[ "$monitor_state" == "right-of" ] && new_monitor_state='left-of'
+[ "$monitor_state" == "left-of" ] && new_monitor_state='same-as'
+[ "$monitor_state" == "same-as" ] && new_monitor_state='right-of'
+
+echo $new_monitor_state > /tmp/i3script_monitorstate
