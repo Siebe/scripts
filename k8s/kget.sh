c41a8f51949afb2637b06534b6bd7a51e636727a
Siebe <siebe@pluxbox.nl>
2024-01-08 09:24:12 +0100

    completely revamped KGet and Klog, wowzers

--- k8s/kget.sh
+++ k8s/kget.sh
@@ -1,15 +1,122 @@
 #!/usr/bin/env bash
 
-pattern=''
-type=''
+usage() {
+  echo 'KGet - Easily find resources in your Kubernetes cluster'
+  echo 'Requires kubectl and jq (for JSON output mode). Usage:'
+  echo  -n './kget.sh [-hjv] -c [-c KUBECTL_CONTEXT] [-n KUBECTL_NAMESPACE] [-k KIND_RESOURCE]'
+  echo ' [-J JSON_EXTRA_MAPPING] [-l LABEL_PATTERN ] [KIND_PATTERN NAME_PATTERN|NAME_PATTERN]'
+  echo 'NAME_PATTERN = regex pattern to filter resource names, case insensitive'
+  echo 'KIND_PATTERN = regex pattern to filter resource kind, case insensitive'
+  echo '-c KUBECTL_CONTEXT = k8s context to use, default is what is current context'
+  echo '-n KUBECTL_NAMESPACE = k8s namespace to use, default is all namespaces option'
+  echo '-k KIND_RESOURCE = what kind of resource to check, default is "all"'
+  echo '-j = JSON output mode (can be slow)'
+  echo '-J JSON_EXTRA_MAPPING = JSON output mode with extra mapping, comma separated'
+  echo '-l LABEL_PATTERN = filter all label values, case insensitive, JSON output mode'
+  echo '-v = verbose mode'
+  echo '-h = display usage and examples'
+}
+
+examples() {
+  echo ''
+  echo 'Examples:'
+  echo 'kubectl get everything, verbose - ./kget.sh- v'
+  echo 'stateful/replica sets, names match "service - ./kget.sh set service'
+  echo 'pods with names matching "traefik" - ./kget.sh -k pod traefik'
+  echo 'pods, sets and jobs as JSON - ./kget.sh -j "(pod|set|job)" .'
+  echo 'pods as JSON with their labels - ./kget.sh -k pod -J "labels:.metadata.labels"'
+  echo 'pods from other context - ./kget.sh -c my-other-cluser -k pods'
+  echo 'deployments from "default" namespace, names match "core" - ./kget.sh -n default -k deploy core'
+  echo 'resources with a label value matching "integration" - ./kget.sh -l integration'
+}
+
+name_pattern=''
+kind_pattern=''
+kind_resource='all'
+namespace_argument='--all-namespaces'
+context_argument=''
+start_pattern='\s+[^\s]*'
+jq_filter='.items[] | '
+json_extra_mapping=''
+
+own_name=$(basename "$0")
+
+KUBECTL_CONTEXT=${KUBECTL_CONTEXT-''}
+KUBECTL_NAMESPACE=${KUBECTL_NAMESPACE-''}
+
+while getopts "hc:n:k:J:l:jv" options
+do
+	case $options in
+		h ) usage; examples; exit 0;;
+    c ) KUBECTL_CONTEXT=${OPTARG};;
+    n ) KUBECTL_NAMESPACE=${OPTARG};;
+    k ) kind_resource="${OPTARG,,}";; #lowercase
+    J ) json_extra_mapping="${OPTARG}";;
+    l ) label_pattern="${OPTARG,,}";; #lowercase
+    j ) json_output=1;;
+    v ) verbose=1;;
+    ? ) echo 'Error: invalid option' >&2; usage; exit 1;;
+	esac
+done
+shift $(($OPTIND - 1))
 
 if [ $# -eq 1 ] ; then
-    pattern=$1
+  #one argument found, pattern for name to lowercase
+  name_pattern="${1,,}"
 fi
 if [ $# -eq 2 ] ; then
-    pattern=$2
-    type=$1
+  #two arguments found, pattern for name and kind to lowercase
+  name_pattern="${2,,}"
+  kind_pattern="${1,,}"
 fi
 
-kubectl get all --all-namespaces | grep -iP "\s+$type.*\/.*$pattern.*\s+" 
+[ -n "$verbose" ] && [ "$name_pattern" != '' ] && echo "Checking name for pattern: '${name_pattern}'" &&\
+  ( [ "$kind_pattern" != '' ] && [ "$kind_resource" == 'all' ] &&\
+    echo "Checking kind for pattern: '${kind_pattern}'" ) ||\
+  { [ -n "$verbose" ] && echo "No patterns to filter"; }
 
+[ "$KUBECTL_CONTEXT" != '' ] && context_argument="--context ${KUBECTL_CONTEXT}" && \
+  { [ -n "$verbose" ] && echo "Checking alternative context: '${KUBECTL_CONTEXT}'"; } || \
+  { [ -n "$verbose" ] && echo "Checking current context: '$(kubectl config current-context)'"; }
+
+[ "$KUBECTL_NAMESPACE" != '' ] && namespace_argument="-n${KUBECTL_NAMESPACE}" && start_pattern='^[^\s]*' &&\
+  { [ -n "$verbose" ] && echo "Checking namespace: '${KUBECTL_NAMESPACE}'"; } ||\
+  { [ -n "$verbose" ] && echo "Checking all namespaces"; }
+
+#Force JSON output for JSON extra mapping
+[ "$json_extra_mapping" != '' ] && json_output=1 &&\
+  [ -n "$verbose" ] && echo "Extra JSON Mapping: ${json_extra_mapping}"
+
+#Force JSON output for label pattern
+[ "$label_pattern" != '' ] && json_output=1 &&\
+  [ -n "$verbose" ] && echo "Checking label for pattern: ${label_pattern}"
+
+if [ -z $json_output ]; then
+  #Assemble grep pattern for regular output
+  grep_filter="${start_pattern}${name_pattern}.*\s+"
+  [ "$kind_resource" = 'all' ] && grep_filter="${start_pattern}${kind_pattern}[^/]*\/[^\s]*${name_pattern}.*\s"
+
+  [ -n "$verbose" ] && echo "Regular output filtered with grep: '${grep_filter}'"
+  command="kubectl get $kind_resource $namespace_argument $context_argument | grep -iP \"$grep_filter\""
+else
+  #Assemble jq filter for JSON output
+  [ "$name_pattern" != '' ] && jq_filter="${jq_filter}select(.metadata.name | test(\"${name_pattern//\\/\\\\}\"; \"i\")) | "
+  [ "$kind_pattern" != '' ] && jq_filter="${jq_filter}select(.kind | test(\"${kind_pattern//\\/\\\\}\"; \"i\")) | "
+  [ "$label_pattern" != '' ] && jq_filter="${jq_filter}select(.metadata.labels | values[] | test(\"${label_pattern//\\/\\\\}\"; \"i\")) | "
+  jq_filter="${jq_filter}{ kind, name: .metadata.name, namespace: .metadata.namespace"
+  [ "$kind_resource" = 'pod' ] || [ "$kind_resource" = 'all' ] && jq_filter="${jq_filter}, podStatus: .status.phase"
+  [ "$kind_resource" = 'replicaset' ] || [ "$kind_resource" = 'deploy' ] || [ "$kind_resource" = 'deployment' ] || \
+    [ "$kind_resource" = 'statefulset' ] || [ "$kind_resource" = 'all' ] && \
+    jq_filter="${jq_filter}, controllerReplicasReady: .status.readyReplicas, controllerReplicas: .status.replicas"
+  [ "$kind_resource" = 'job' ] || [ "$kind_resource" = 'all' ] && jq_filter="${jq_filter}, jobSucceeded: .status.succeeded"
+  [ "$json_extra_mapping" != '' ] && jq_filter="${jq_filter}, ${json_extra_mapping}"
+  jq_filter="${jq_filter} }"
+
+  [ -n "$verbose" ] && echo "JSON output filtered with jq: '${jq_filter}'"
+  command="kubectl get $kind_resource $namespace_argument $context_argument -o=json | jq '$jq_filter'"
+fi
+
+[ -n "$verbose" ] && echo "Full command: ${command}"
+[ -n "$verbose" ] && echo "============= Running"
+bash -c "$command"
+[ -n "$verbose" ] && echo "============= ktnxbye"
--- k8s/klog.sh
+++ k8s/klog.sh
@@ -1,5 +1,222 @@
 #!/usr/bin/env bash
 
-entry=(`kubectl get pods --all-namespaces | grep $1 | head -n1`)
-echo --- Logs from ${entry[1]} \(${entry[0]}\): ---
-kubectl logs -f -n${entry[0]} ${entry[1]} --all-containers
+usage() {
+  echo 'KLog - Easily log multiple pods in your Kubernetes cluster'
+  echo 'Requires kubectl, jq and "kget.sh" in the $SCRIPT_DIR or same directory. Usage:'
+  echo  -n './klog.sh [-hlv] -c [-c KUBECTL_CONTEXT] [-o CONTAINER] [-t TAIL_LINES ]'
+  echo ' [-s SCAN_INTERVAL] [-m MAXIMUM_PODS] [KUBECTL_NAMESPACE] [INPUT_PATTERN]'
+  echo 'KUBECTL_NAMESPACE = k8s namespace to use, default is "default"'
+  echo 'INPUT_PATTERN = regex pattern to filter pod names or labels, case insensitive'
+  echo '-c KUBECTL_CONTEXT = k8s context to use, default is current context'
+  echo '-o CONTAINER = specific name of container to log in pods, default is all containers'
+  echo '-t TAIL_LINES = number of log lines to output initially per pod, default is -1 (all)'
+  echo -n '-s SCAN_INTERVAL = number of seconds to scan for new/restarted pods in a loop. '
+  echo '0 means scan only once, default is to loop every 30 seconds'
+  echo '-m MAXIMUM_PODS = maximum number of processes spawned to log pods, default is 10'
+  echo '-l = filter on all label values instead of pod name, case insensitive'
+  echo '-v = verbose mode'
+  echo '-h = display usage and examples'
+}
+
+examples() {
+  echo ''
+  echo 'Examples:'
+  echo 'Log no more than 20 pods from "databases" namespace:'
+  echo './klog.sh -m 20 databases'
+  echo ''
+  echo 'Log no more than 10 pods from "databases" with names matching "mongodb", verbose mode:'
+  echo './klog.sh -v databases mongodb'
+  echo ''
+  echo 'Log pods from "workers" namespaces, with label value matching "nginx", select "webserver" container:'
+  echo './klog.sh -l -o webserver workers nginx'
+  echo ''
+  echo 'Log pods from "workers" namespace, "my-cluster" context and scan for new pods every 2 minutes:'
+  echo './klog.sh -c my-cluster -s 120 workers nginx'
+  echo''
+  echo 'Log pods from "monitoring" namespace, pods names start with "server", don''t scan for new pods'
+  echo './klog.sh -s 0 monitoring "\bserver"'
+}
+
+## Variables ##
+
+KUBECTL_CONTEXT=${KUBECTL_CONTEXT-''}
+KUBECTL_NAMESPACE=${KUBECTL_NAMESPACE-'default'}
+[ -n "$SCRIPT_DIR" ] && kget_executable="${SCRIPT_DIR}/k8s/kget.sh" || \
+  kget_executable="$(dirname -- "$( readlink -f -- "$0"; )")/kget.sh"
+kget_arguments=''
+kget_command=''
+log_arguments=''
+log_command=''
+input_pattern=''
+container=''
+tail='-1'
+scan_interval='30'
+process_was_running=1
+maximum_pods=10
+
+log_pids=()
+log_pods=()
+log_index=0
+
+## Functions ##
+
+array_find() {
+  find_value="$1"
+  shift
+  array=("$@")
+  for index in "${!array[@]}"; do
+     if [ "${array[$index]}" = "${find_value}" ]; then
+         echo "${index}";
+         return
+     fi
+  done
+  echo "-1"
+}
+
+array_remove_index() {
+  remove_index="$1"
+  shift
+  array=("$@")
+  output=()
+  for index in "${!array[@]}"; do
+    [ "$index" != "${remove_index}" ] && output+=("${array[$index]}")
+  done
+  echo "${output[@]}"
+}
+
+log_template_json() {
+  while read log_logline; do
+    log_line_escaped="${log_logline//\\/\\\\}"
+    log_line_escaped="${log_line_escaped//\"/\\\"}"
+    echo "{\"name\":\"${1}\",\"log\":\"${log_line_escaped}\"}"
+  done
+}
+
+scan_pods_and_log() {
+  [ -n "$verbose" ] && echo "Scan pods with command: ${kget_command}"
+  podlist=( $(bash -c "$kget_command") )
+
+  for pod in "${podlist[@]}"; do
+    [ -n "$verbose" ] && echo "Checking log process for ${pod}"
+    existing_index='-1'
+    existing_index=$(array_find "$pod" "${log_pods[@]}")
+    if [ "$existing_index" -gt -1 ]; then
+      pid=${log_pids[${existing_index}]}
+      if ps -p $pid > /dev/null; then
+        [ -n "$verbose" ] && echo "Already logging ${pod}"
+        continue;
+      fi
+      [ -n "$verbose" ] && echo "Processs stopped for ${pod}, trying again."
+      log_pids=( $(array_remove_index "$existing_index" "${log_pids[@]}") )
+      log_pods=( $(array_remove_index "$existing_index" "${log_pods[@]}") )
+    fi
+    if [ "${#log_pods[@]}" -ge "$maximum_pods" ]; then
+      [ -n "$verbose" ] && echo "Already logging maximum amount of pods"
+      continue;
+    fi
+    log_command="kubectl logs -f${log_arguments} $pod"
+    [ -n "$verbose" ] && echo "Spawning log process with command: ${log_command}"
+    bash -c "$log_command" | log_template_json $pod &
+    log_pids[${log_index}]=$!
+    log_pods[${log_index}]=$pod
+    log_index=$((log_index+1))
+  done;
+}
+
+start_scan_loop() {
+  while true; do
+    [ -n "$verbose" ] && echo "Scan loop: waiting ${scan_interval} seconds"
+    sleep "$scan_interval"
+    [ -n "$verbose" ] && echo "Scan loop: scan for pods again"
+    scan_pods_and_log
+  done
+}
+
+## Argument processing ##
+
+while getopts "hc:o:t:s:m:lv" options
+do
+	case $options in
+		h ) usage; examples; exit 0;;
+    c ) KUBECTL_CONTEXT=${OPTARG};;
+    o ) container=${OPTARG};;
+    t ) tail=${OPTARG};;
+    s ) scan_interval=${OPTARG};;
+    m ) maximum_pods=${OPTARG};;
+    l ) label_pattern=1;; #lowercase
+    v ) verbose=1;;
+    ? ) echo 'Error: invalid option' >&2; usage; exit 1;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [ $# -ge 1 ] ; then
+  #one argument found, namespace to lowercase
+  KUBECTL_NAMESPACE="${1,,}"
+fi
+if [ $# -ge 2 ] ; then
+  #two arguments found, pattern to lowercase
+  input_pattern="${2,,}"
+fi
+
+re='^[0-9]+$'
+if ! [[ $scan_interval =~ $re ]] ; then
+   echo "error: SCAN_INTERVAL is not a number" >&2; exit 1
+fi
+if ! [[ $maximum_pods =~ $re ]] ; then
+   echo "error: MAXIMUM_PODS is not a number" >&2; exit 1
+fi
+
+if [ -n "$verbose" ]; then
+  [ "$KUBECTL_CONTEXT" != '' ] &&  echo "Alternative k8s context: ${KUBECTL_CONTEXT}" ||\
+    echo "Current K8s context: $(kubectl config current-context)"
+  echo "K8s namespace: ${KUBECTL_NAMESPACE}";
+  echo "Pattern to search for: ${input_pattern}"
+  [ -n "$label_pattern" ] && echo "Search in label values of pods";
+  [ "$container" != '' ] && echo "Log container: ${container}" || echo "Log all containers"
+fi
+
+[ "$KUBECTL_CONTEXT" != '' ] && kget_arguments="${kget_arguments} --context ${KUBECTL_CONTEXT}"
+kget_arguments="${kget_arguments} -n ${KUBECTL_NAMESPACE}"
+
+log_arguments="$kget_arguments"
+
+[ -n "$label_pattern" ] && kget_arguments="${kget_arguments} -l" && \\
+
+[ "$container" != '' ] && log_arguments="${log_arguments} -c ${container}" || \
+  log_arguments="${log_arguments} --all-containers"
+log_arguments="${log_arguments} --tail ${tail}"
+
+kget_command="${kget_executable} -k pod -j${kget_arguments} \"${input_pattern}\" | \
+jq -r 'select(.podStatus == \"Running\") | .name' | sed 's/\\n/ /'"
+
+## Scan and Log ##
+
+[ -n "$verbose" ] && echo "Starting initial Scan for pods";
+scan_pods_and_log
+[ "${#log_pods[@]}" -eq 0 ] && { echo "No pods found in ${KUBECTL_NAMESPACE} namespace" >&2; exit 1; }
+
+[ "$scan_interval" -ge 1 ] && { [ -n "$verbose" ] && echo "Starting Scan loop"; start_scan_loop & }
+
+#https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+## Keep alive  ##
+
+while [ $process_was_running -eq 1 ]; do
+  # wait for all pids
+  process_was_running=0
+  for pid in ${log_pids[*]}; do
+      [ "$pid" == '' ] && continue
+      [ "$scan_interval" -ge 1 ] && ps -p $pid > /dev/null && process_was_running=1
+      wait $pid
+  done
+  if [ $process_was_running -eq 1 ]; then
+    [ -n "$verbose" ] && echo "All logging processes stopped, scanning again in ${scan_interval} seconds";
+    sleep "$scan_interval"
+    scan_pods_and_log
+  fi
+done
+
+[ -n "$verbose" ] && echo "Exiting normally, all log processes ended"
+exit 0
